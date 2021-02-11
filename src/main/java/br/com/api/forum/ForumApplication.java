0b835233a088eaa5a2a87deb141d30b0b7dac145
Elias Lima <eliaslima900@hotmail.com>
2021-02-11 12:59:00 -0300

    initial commits

---
+++ src/main/java/br/com/api/forum/ForumApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
+	}
+
+}
